97c66043a852e27062d7338fa206d5b8f8c3f26e
Raxit Gohel <ragohel0101@gmail.com>
2024-01-17 17:27:27 +0530

    modified other question

--- q1.py
+++ q1.py
@@ -7,3 +7,10 @@ ordered_list = []
 for i in range(n):
     if prereqs[i]!='None' and prereqs[i] not in courses:
         ordered_list.append(prereqs[i])
+        prereqs[i] = 'None'
+    
+for i in courses:
+    if i not in ordered_list:
+        ordered_list.append(i)
+print(ordered_list)
+    
