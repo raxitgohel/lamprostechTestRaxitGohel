f96dd88571052468ab219ce7d58aad36bfa374bd
Raxit Gohel <ragohel0101@gmail.com>
2024-01-17 17:21:07 +0530

    added other question

--- q1.py
+++ q1.py
@@ -1,1 +1,9 @@
-courses = input().split(' ')
+# Get neccessary inputs. Write 'None' if a course doesn't have prerequisite.
+courses = input().split(' ') 
+prereqs = input().split(' ') 
+
+n = len(courses)
+ordered_list = []
+for i in range(n):
+    if prereqs[i]!='None' and prereqs[i] not in courses:
+        ordered_list.append(prereqs[i])
